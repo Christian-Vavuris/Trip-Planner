5303af55931afa05dfbc7cccf65b3512daf73002
Maximo100100 <Maximoabahamondes@gmail.com>
2020-07-03 10:37:37 -0700

    APIKey added

---
+++ assets/js/search.js
@@ -0,0 +1,14 @@
+const FusionKey = "aj1aJ-z1e6b_O1mxu6a8UPQ0HeoqdxsrBgHZ3r3sm8rLUKcDkAv_m5ybNOOvpyRDkYl7BFYzy8dUT9GGRT2Go0-dkJK3uJ-uQsNeccbQWuO7tsCIbI8tBdS9nWr_XnYx"
+const ClientID = "jM2lfutqm_wP0Esinv1aHQ"
+
+var ContainerEl = document.getElementById("result-container");
+
+var searchCity = function(){
+    var userCity = window.prompt("Test (enter city)")
+
+
+
+
+
+};
+searchCity();
