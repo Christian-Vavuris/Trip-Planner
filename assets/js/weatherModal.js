773f2ca992360f10477773d7adf842e97684303d
shhu21 <hu.sara21@gmail.com>
2020-07-05 03:26:09 -0700

    adds template code

--- assets/js/weatherModal.js
+++ assets/js/weatherModal.js
@@ -1,13 +1,22 @@
+function callAPI(url, callBack) {
+    fetch(url).then(function(response) {
+        if(response.ok) {
+            response.json().then(function (response) {
+                callBack(response);
+            });
+        }
+    })
+}
+
 function openWModal(place) {
-    $('.modal').css('display', 'block');
+    $('.weather-modal').css('display', 'block');
     // $('.modal').addClass('show-modal');
 
-    var placeInfo = JSON.parse(localStorage.getItem(place.innerHTML));
+    var placeInfo = JSON.parse(localStorage.getItem(place.textContent));
     $('.modal-header').text(placeInfo.place);
 
-    // create the API URL using placeInfo.location
-    // call the API
-        // callback weatherInfo
+    var url = `https://api.openweathermap.org/data/2.5/weather?appid=640b3bbebec045da381544940d161ab8&q=${place.location}&units=imperial`;
+    callAPI(url, weatherInfo);
 }
 
 function closeWModal() {
@@ -16,8 +25,14 @@ function closeWModal() {
     // $('.modal').removeClass('show-modal');
 }
 
-function weatherInfo() {
-    
+function weatherInfo(data) {
+    $('#temp').text(`Temperature: ${data.main.temp}°F`);
+    $('#icon').attr('src', `http://openweathermap.org/img/w/${data.weather[0].icon}.png`);
+    $('#temp-max').text(`High: ${data.main.temp_max}°F`)
+    $('#temp-min').text(`Low: ${data.main.temp_min}°F`)
+    $('#humidity').text(`Humidity: ${data.main.humidity}%`);
+    $('wind').text(`Wind Speed: ${data.wind.speed} MPH`);
 }
 
 $('#weather-close').on('click', closeWModal);
+$('#open').on('click', openWModal);
