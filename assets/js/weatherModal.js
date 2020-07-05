75c7f19490be10e37f0d65441d1a53b773cb0cc7
shhu21 <hu.sara21@gmail.com>
2020-07-05 03:46:45 -0700

    changes id names

--- assets/js/weatherModal.js
+++ assets/js/weatherModal.js
@@ -8,20 +8,21 @@ function callAPI(url, callBack) {
     })
 }
 
-function openWModal(place) {
-    $('.weather-modal').css('display', 'block');
+function openModal(place) {
+    $('.modal').css('display', 'block');
     // $('.modal').addClass('show-modal');
 
     var placeInfo = JSON.parse(localStorage.getItem(place.textContent));
-    $('.modal-header').text(placeInfo.place);
+    $('#title').text(placeInfo.place);
 
-    var url = `https://api.openweathermap.org/data/2.5/weather?appid=640b3bbebec045da381544940d161ab8&q=${place.location}&units=imperial`;
+    // TODO: probably change out API ID to use the same as the other one
+    var url = `https://api.openweathermap.org/data/2.5/weather?appid=640b3bbebec045da381544940d161ab8&q=${placeInfo.location}&units=imperial`;
     callAPI(url, weatherInfo);
 }
 
-function closeWModal() {
+function closeModal() {
     // close the modal
-    $('.weather-modal').css('display', 'none');
+    $('.modal').css('display', 'none');
     // $('.modal').removeClass('show-modal');
 }
 
@@ -31,8 +32,8 @@ function weatherInfo(data) {
     $('#temp-max').text(`High: ${data.main.temp_max}°F`)
     $('#temp-min').text(`Low: ${data.main.temp_min}°F`)
     $('#humidity').text(`Humidity: ${data.main.humidity}%`);
-    $('wind').text(`Wind Speed: ${data.wind.speed} MPH`);
+    $('#wind').text(`Wind Speed: ${data.wind.speed} MPH`);
 }
 
-$('#weather-close').on('click', closeWModal);
-$('#open').on('click', openWModal);
+$('#close').on('click', closeModal);
+$('#open').on('click', openModal);
